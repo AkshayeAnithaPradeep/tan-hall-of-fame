14d2496baa6d76c593cddc106fa4c3bd6c0dcb5f
Akshaye <akshaye07@gmail.com>
2023-08-06 18:25:18 -0400

    Added maxi

--- src/Notes.js
+++ src/Notes.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import { motion, useAnimate } from "framer-motion"
 import { makeStyles } from '@material-ui/core/styles';
 import { css } from "@emotion/core";
 import PulseLoader from "react-spinners/PulseLoader";
 import Note from "./Note";
+import meow from "./audio/meow.mp3";
 
 const override = css`
   display: block;
@@ -16,6 +18,7 @@ const override = css`
 const useStyles = makeStyles((theme) => ({
     notes: {
         position: "relative",
+        overflow: 'hidden',
         display: "flex",
         flexWrap: "wrap",
         justifyContent: "space-around",
@@ -32,13 +35,65 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
         textAlign: "center"
     },
+    cat: {
+        position: 'absolute',
+        left: 0,
+        top: 50,
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    },
+    catImageFlipped: {
+        transform: 'rotateY(180deg)'
+    },
+    catImage: {
+        transform: 'rotateY(0deg)'
+    }
 }));
 
 export default function Notes(props) {
     const classes = useStyles();
+    const [height, setHeight] = useState(0)
+    const [posY, setPosY] = useState('100px');
+    const [scope, animate] = useAnimate()
+    const ref = useRef(null)
+
+    useEffect(() => {
+        setHeight(ref.current.clientHeight);
+        let highest = -500, lowest = window.innerWidth+(0.10 * window.innerWidth), going = 'right';
+        let posYTemp;
+        animate(scope.current, {x:[-500, window.innerWidth+(0.10 * window.innerWidth)]} , { repeat: Infinity, duration: 20, repeatType: "mirror", onUpdate:(latest) => {
+            let cat = document.getElementById("cat");
+            if (going === 'right') {
+                highest = latest > highest ? latest : highest;
+                if (latest < highest) {
+                    going = 'left';
+                    highest = -500;
+                    cat.className = classes.catImageFlipped;
+                    posYTemp = `${Math.floor(Math.random() * height)}px`;
+                    animate(scope.current, {y:posYTemp});
+                }
+            } else {
+                lowest = latest < lowest ? latest : lowest;
+                if ( latest > lowest) {
+                    going = 'right';
+                    lowest = window.innerWidth+(0.10 * window.innerWidth);
+                    cat.className = classes.catImage;
+                    posYTemp = `${Math.floor(Math.random() * height)}px`;
+                    animate(scope.current, {y:posYTemp});
+                }
+            }
+            animate(scope.current)
+        } });
+    });
+
+    function playMeow() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
 
     return (
-        <div className={classes.notes}>
+        <div className={classes.notes} ref={ref}>
             <PulseLoader
                 css={override}
                 size={10}
@@ -46,6 +101,13 @@ export default function Notes(props) {
                 loading={props.loading}
             />
             {props.notes.map((note, key) => <Note key={key} note={note}/> )}
+            {/* <div className={classes.cat} id='cat-container'>
+                <img id="cat" src="https://www.kasandbox.org/programming-images/misc/cat-walk.gif"/>
+            </div> */}
+            <motion.div className={classes.cat} ref={scope} style={{top: posY}} onClick={playMeow}>
+                <img id="cat" src="https://www.kasandbox.org/programming-images/misc/cat-walk.gif"/>
+            </motion.div >
+            <audio id="audio" src={meow}></audio>
         </div>
     );
 }
