7a58157eab7b52e09f963372c2e373ef2697c592
Akshaye Anitha Pradeep <aanithap@mathworks.com>
2026-08-06 08:52:07 -0500

    Add hover to edit button

--- src/Note.jsx
+++ src/Note.jsx
@@ -34,7 +34,7 @@ export default function Note(props) {
     if (hasImage) {
         return (
             <div className="w-[calc(25%-60px)] min-w-[280px] rounded-xl shadow-lg overflow-hidden bg-white flex flex-col relative">
-                <button onClick={() => setOpen(true)} className="absolute top-2 right-2 p-1.5 bg-white/70 hover:bg-white rounded-full shadow-sm">
+                <button onClick={() => setOpen(true)} className="absolute top-2 right-2 z-[5] p-1.5 bg-white/70 hover:bg-white hover:scale-110 rounded-full shadow-sm cursor-pointer transition-transform">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z"/></svg>
                 </button>
                 <PhotoProvider>
@@ -81,7 +81,7 @@ export default function Note(props) {
                 backgroundSize: '50%',
             }}
         >
-            <button onClick={() => setOpen(true)} className="absolute top-2 right-2 p-1.5 hover:bg-black/10 rounded-full">
+            <button onClick={() => setOpen(true)} className="absolute top-2 right-2 p-1.5 hover:bg-black/10 hover:scale-110 rounded-full cursor-pointer transition-transform">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75z"/></svg>
             </button>
             <div className="mb-3 flex-grow flex items-center">
